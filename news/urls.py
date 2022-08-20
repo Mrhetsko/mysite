258b77985507707f4a3e63d220bd0fb913a9d8ff
Dmytro Hetsko <49610661+Mrhetsko@users.noreply.github.com>
2022-08-20 13:28:29 +0200

    second backup

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('category/<int:category_id>/', views.get_category, name='category'),
 ]
