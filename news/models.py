c2ff03adc72791d1ab58a046595841814e2ebe8d
Dmytro Hetsko <49610661+Mrhetsko@users.noreply.github.com>
2022-08-22 12:53:52 +0200

    Finish second form lesson

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -6,9 +7,12 @@ class News(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True, verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
     def __str__(self):
         return self.title
@@ -24,6 +28,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     class Meta:
         verbose_name = 'Категория'
--- news/views.py
+++ news/views.py
@@ -1,30 +1,66 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
+from .forms import NewsForm
 
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
+
     title = 'Новости'
     context = {
         'title': title,
         'news': news,
-        'categories': categories
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, 'news/category.html', context)
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    context = {
+        'news_item': news_item,
+    }
+    return render(request, 'news/view_news.html', context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
+
+    else:
+        form = NewsForm()
+    context = {
+        'form': form,
+        }
+    return render(request, 'news/add_news.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
